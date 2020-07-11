c4aef52ee293e16430cb256750b78f51be172816
Samuel Piatanesi <piatanesi.samuel@gmail.com>
2020-07-11 11:55:17 +0200

    added toast

--- App.js
+++ App.js
@@ -83,9 +83,11 @@ export default class App extends Component<{}> {
         //Se è lungo 16 significa che è l'id iniziale del device, altrimenti è il messaggio
         if (receivedData.length == 16) {
           receivedId = receivedData;
+          ToastAndroid.show(`Connesso al dispositivo ${receivedId}`, ToastAndroid.SHORT);
           console.log("Id dispositivo: ${receivedId}")
         } else {
           receivedMessage = receivedData;
+          ToastAndroid.show(`Connesso al dispositivo ${receivedMessage}`, ToastAndroid.SHORT);
           console.log("Id dispositivo: ${receivedMessage}")
         }
       });
